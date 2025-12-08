f34d3ccb6fa052650ea02523040277b5df7b0cbd
czdrxne <czdrxne@gmail.com>
2025-12-08 21:31:25 +0800

    Development update. Added necessary content notice for ongoing website construction.

---
+++ src/lib/routes/Home.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	
+</script>
+
+<section
+	id="home"
+	class="relative h-svh w-full overflow-hidden"
+>
+	<div class="h-full flex items-center justify-center flex-col font-secondary gap-10 pt-[3rem]">
+		<h1 class="font-black text-wht-pri text-4xl sm:text-[9svw] my-[-5svh]">
+			<span class="text-blu-pri">COMING</span>
+			<span>SOON</span>
+		</h1>
+		<p class="text-center text-wht-pri text-[0.8rem] sm:text-[2svw]">\\ WEBSITE UNDER CONSTRUCTION //</p>
+		<a
+			href=""
+			rel="noreferrer noopener"
+			title="reach out"
+			class="relative bg-blu-pri px-3 py-1.5 sm:px-6 sm:py-2 font-[600] text-[0.6rem] md:text-[1.2svw] cursor-pointer text-wht-pri overflow-hidden transition-colors duration-300 before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-full before:h-0 before:bg-wht-pri before:transition-all before:duration-200 before:z-[1] hover:before:h-full hover:text-blu-pri flex items-center"
+		>
+			<span class="relative z-[2]">REACH OUT</span>
+		</a>
+
+	</div>
+</section>
