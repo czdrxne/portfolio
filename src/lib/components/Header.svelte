1fa17fdcbfe337f2ecdd790389bfd357f145491e
czdrxne <czdrxne@gmail.com>
2025-12-08 21:32:21 +0800

    Development update. Added header section for user accessibility.

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	
+</script>
+
+<header
+	id="header"
+	class="fixed z-[100] w-full overflow-hidden"
+>
+	<div class="sm:py-6 sm:px-12 py-2.5 px-4 flex justify-between items-center cursor-default">
+		<a
+			href="https://www.linkedin.com/in/czdrxne/"
+			title="linkedin"
+			target="_blank"
+			rel="noreferrer noopener"
+			class="cursor-pointer"
+		>
+			<h1
+				class="font-primary text-wht-pri sm:text-2xl text-lg"
+			>CZDRXNE</h1>
+		</a>
+
+		<a
+			href="https://github.com/czdrxne/"
+			title="github"
+			target="_blank"
+			rel="noreferrer noopener"
+			class="cursor-pointer"
+		>
+			<img
+				src="/images/logo.webp"
+				alt="logo image"
+				class="sm:w-[40px] w-[25px]"
+			/>
+		</a>
+	</div>
+</header>
