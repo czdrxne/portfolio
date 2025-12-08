2d0a65aafa7dbe8ef2689fa1b317a91047c241cd
czdrxne <czdrxne@gmail.com>
2025-12-08 21:34:13 +0800

    Development update. Added proper styling and added necesssary components ensuring excellent system structure for future development.

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  
+  import Preloader 			from "@components/Preloader.svelte";
+  import Header 				from "@components/Header.svelte";
+
+  import Home 					from "@routes/Home.svelte";
+  
+  let showPreloader: boolean = true;
+
+</script>
+
+<Preloader
+  bind:showPreloader
+  on:done={() => showPreloader = false}
+/>
+<div>
+	<Header />
+	<main class="relative h-svh w-svw bg-blk-pri cursor-default overflow-hidden selection:bg-blu-pri selection:text-wht-pri">
+  	<Home />
+	</main>
+</div>
