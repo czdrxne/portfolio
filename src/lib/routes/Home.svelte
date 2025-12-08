fcce353b7055f8f5b48cd403389d9109c1b9cb52
czdrxne <czdrxne@gmail.com>
2025-12-08 21:43:30 +0800

    Development update. Added crucial anchor tag href for mailing purposes.

--- src/lib/routes/Home.svelte
+++ src/lib/routes/Home.svelte
@@ -13,7 +13,8 @@
 		</h1>
 		<p class="text-center text-wht-pri text-[0.8rem] sm:text-[2svw]">\\ WEBSITE UNDER CONSTRUCTION //</p>
 		<a
-			href=""
+			href="mailto:czdrxne@gmail.com?subject=Hello%20Louise%20Czedrian&body=Hi%20Louise,%0A%0AI%20came%20across%20your%20portfolio%20website%20while%20it%E2%80%99s%20still%20in%20development,%20and%20I%20wanted%20to%20reach%20out.%20It%20looks%20like%20you%E2%80%99re%20working%20on%20something%20nice,%20and%20I%20thought%20it%20would%20be%20a%20good%20opportunity%20to%20connect.%0A%0AI%E2%80%99m%20interested%20in%20what%20you%E2%80%99re%20putting%20together%20and%20would%20like%20to%20know%20more%20about%20your%20plans%20for%20the%20site%20once%20it%E2%80%99s%20finished.%20If%20you%E2%80%99re%20open%20to%20it,%20I%E2%80%99d%20be%20happy%20to%20chat%20or%20exchange%20a%20few%20ideas.%0A%0AHope%20to%20hear%20from%20you%20soon.%0A%0ABest,%0A[Your%20Name]"
+			target="_blank"
 			rel="noreferrer noopener"
 			title="reach out"
 			class="relative bg-blu-pri px-3 py-1.5 sm:px-6 sm:py-2 font-[600] text-[0.6rem] md:text-[1.2svw] cursor-pointer text-wht-pri overflow-hidden transition-colors duration-300 before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-full before:h-0 before:bg-wht-pri before:transition-all before:duration-200 before:z-[1] hover:before:h-full hover:text-blu-pri flex items-center"
